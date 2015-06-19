b107b9386d8d9c46efa0496de0fc24c1f1f9eb20
darkevilmac <bk1325@gmail.com>
2015-06-19 15:51:34 -0700

    Correct version number, add MixinEntity so we can work on custom collision.

---
+++ src/main/java/darkevilmac/movingworld/asm/mixin/core/entity/MixinEntity.java
@@ -0,0 +1,14 @@
+package darkevilmac.movingworld.asm.mixin.core.entity;
+
+import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+
+/**
+ * Override minecraft's move methods so we can tell it how to handle collision.
+ */
+@Mixin(Entity.class)
+public class MixinEntity {
+
+
+
+}
