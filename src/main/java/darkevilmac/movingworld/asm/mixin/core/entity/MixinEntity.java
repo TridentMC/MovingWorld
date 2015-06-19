aab3a75588ae1a96cd53527d4368e59398d28c6e
darkevilmac <bk1325@gmail.com>
2015-06-19 15:54:57 -0700

    Add moveEntity injection

--- src/main/java/darkevilmac/movingworld/asm/mixin/core/entity/MixinEntity.java
+++ src/main/java/darkevilmac/movingworld/asm/mixin/core/entity/MixinEntity.java
@@ -2,6 +2,9 @@ package darkevilmac.movingworld.asm.mixin.core.entity;
 
 import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  * Override minecraft's move methods so we can tell it how to handle collision.
@@ -9,6 +12,9 @@ import org.spongepowered.asm.mixin.Mixin;
 @Mixin(Entity.class)
 public class MixinEntity {
 
-
+    @Inject(method = "moveEntity(DDD)V", at = @At("HEAD"), cancellable = true)
+    public void onMoveEntity(double x, double y, double z, CallbackInfo ci) {
+        // Does nothing currently.
+    }
 
 }
