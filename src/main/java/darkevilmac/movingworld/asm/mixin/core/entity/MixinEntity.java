0ae5bbf6d334033dc9408c1ecd7622a3e94bbf23
darkevilmac <bk1325@gmail.com>
2015-06-19 18:14:50 -0700

    Add nearby ship detection to moveEntity

--- src/main/java/darkevilmac/movingworld/asm/mixin/core/entity/MixinEntity.java
+++ src/main/java/darkevilmac/movingworld/asm/mixin/core/entity/MixinEntity.java
@@ -1,20 +1,37 @@
 package darkevilmac.movingworld.asm.mixin.core.entity;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.List;
+
 /**
- * Override minecraft's move methods so we can tell it how to handle collision.
+ * Override Minecraft's move methods so we can tell it how to handle collision.
  */
 @Mixin(Entity.class)
 public class MixinEntity {
 
-    @Inject(method = "moveEntity(DDD)V", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "moveEntity(DDD)V", at = @At("INVOKE"), cancellable = true)
     public void onMoveEntity(double x, double y, double z, CallbackInfo ci) {
-        // Does nothing currently.
+        Entity thisEntity = (Entity) (Object) this;
+        World worldObj = thisEntity.worldObj;
+
+        // If this entity is in noClip just let the method do it's stuff.
+        if (thisEntity.noClip)
+            return;
+
+        AxisAlignedBB boundingBox = new AxisAlignedBB(thisEntity.posX - 3, thisEntity.posY - 5, thisEntity.posZ - 3,
+                thisEntity.posX + 3, thisEntity.posY + 5, thisEntity.posZ + 3);
+        List<Entity> movingWorldsInBox = worldObj.getEntitiesWithinAABBExcludingEntity(thisEntity, boundingBox);
+
+        if (movingWorldsInBox != null && !movingWorldsInBox.isEmpty()) {
+
+        }
     }
 
 }
