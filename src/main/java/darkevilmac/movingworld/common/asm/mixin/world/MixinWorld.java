a389ffac2d48ec4846e635a7b3fc329575f92f17
darkevilmac <bk1325@gmail.com>
2016-01-22 15:22:39 -0800

    Add Callbackinfo so we don't crash on startup which is nice.

--- src/main/java/darkevilmac/movingworld/common/asm/mixin/world/MixinWorld.java
+++ src/main/java/darkevilmac/movingworld/common/asm/mixin/world/MixinWorld.java
@@ -10,6 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.HashMap;
 import java.util.List;
@@ -59,7 +60,7 @@ public class MixinWorld implements IWorldMixin {
     }
 
     @Inject(method = "updateEntities", at = @At("RETURN"))
-    public void onUpdateEntities() {
+    public void onUpdateEntities(CallbackInfo cbi) {
         for (SubWorld sub : subworlds.values()) {
             sub.updateEntities();
         }
