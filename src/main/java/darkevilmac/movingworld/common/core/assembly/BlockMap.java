820fae82f80f4558dfc724b8e4545d39c4f06da3
darkevilmac <bk1325@gmail.com>
2016-01-23 02:16:56 -0800

    Make the BlockMap a bit nicer.

--- src/main/java/darkevilmac/movingworld/common/core/assembly/BlockMap.java
+++ src/main/java/darkevilmac/movingworld/common/core/assembly/BlockMap.java
@@ -6,13 +6,15 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockPos;
 import org.apache.commons.lang3.tuple.Pair;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Get a blockstate and a tile (if applicable) from a position. Contains some nice utility methods.
  */
-public class BlockMap implements Iterable<Pair<BlockPos, Pair<IBlockState, TileEntity>>> {
+public class BlockMap implements Iterable<Pair<IBlockState, TileEntity>> {
 
     private HashMap<BlockPos, Pair<IBlockState, TileEntity>> internalMap;
 
@@ -21,8 +23,8 @@ public class BlockMap implements Iterable<Pair<BlockPos, Pair<IBlockState, TileE
     }
 
     @Override
-    public Iterator<Pair<BlockPos, Pair<IBlockState, TileEntity>>> iterator() {
-        return null;
+    public Iterator<Pair<IBlockState, TileEntity>> iterator() {
+        return new BlockMapIterator();
     }
 
     public IBlockState getBlockState(BlockPos key) {
@@ -33,4 +35,32 @@ public class BlockMap implements Iterable<Pair<BlockPos, Pair<IBlockState, TileE
         return internalMap.get(key).getRight();
     }
 
+    protected class BlockMapIterator implements Iterator<Pair<IBlockState, TileEntity>> {
+        int index;
+        List<BlockPos> keySet = new ArrayList<BlockPos>();
+
+        public BlockMapIterator() {
+            index = 0;
+            keySet.addAll(internalMap.keySet());
+        }
+
+        @Override
+        public boolean hasNext() {
+            return index <= internalMap.size();
+        }
+
+        @Override
+        public Pair<IBlockState, TileEntity> next() {
+            Pair<IBlockState, TileEntity> pair = internalMap.get(keySet.get(index));
+            index++;
+            return pair;
+        }
+
+        @Override
+        public void remove() {
+            internalMap.remove(keySet.get(index));
+            keySet.remove(index);
+            index--;
+        }
+    }
 }
