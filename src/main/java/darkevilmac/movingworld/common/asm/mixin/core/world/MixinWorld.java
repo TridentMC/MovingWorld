fec4cd88dbc6a12cb766d6f5496ff15f6346e492
darkevilmac <bk1325@gmail.com>
2016-04-19 01:48:31 -0700

    Just don't bother with the collision injection if we're not using it.

--- src/main/java/darkevilmac/movingworld/common/asm/mixin/core/world/MixinWorld.java
+++ src/main/java/darkevilmac/movingworld/common/asm/mixin/core/world/MixinWorld.java
@@ -1,19 +1,12 @@
 package darkevilmac.movingworld.common.asm.mixin.core.world;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.List;
 
 @Mixin(World.class)
 public class MixinWorld {
-    @Inject(method = "getCollisionBoxes(Lnet/minecraft/entity/Entity;Lnet/minecraft/util/AxisAlignedBB;)Ljava/util/List;", at = @At("RETURN"), cancellable = true)
-    public void onGetCollisionBoxes(Entity source, AxisAlignedBB within, CallbackInfoReturnable<List> cbr) {
-        //TODO: Experiment with getting ship collision boxes
-    }
+    //@Inject(method = "getCollisionBoxes(Lnet/minecraft/entity/Entity;Lnet/minecraft/util/AxisAlignedBB;)Ljava/util/List;", at = @At("RETURN"), cancellable = true)
+    //public void onGetCollisionBoxes(Entity source, AxisAlignedBB within, CallbackInfoReturnable<List> cbr) {
+    //    //TODO: Experiment with getting ship collision boxes
+    //}
 }
