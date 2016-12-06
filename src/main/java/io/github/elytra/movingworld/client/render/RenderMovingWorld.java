b7c7a77a9acad4c6ae5765ec344bafc9a420ff11
darkevilmac <bk1325@gmail.com>
2016-12-05 19:45:48 -0800

    Use the <T> in Render<T>

--- src/main/java/io/github/elytra/movingworld/client/render/RenderMovingWorld.java
+++ src/main/java/io/github/elytra/movingworld/client/render/RenderMovingWorld.java
@@ -5,18 +5,25 @@ import net.minecraft.client.renderer.culling.ICamera;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.texture.TextureMap;
-import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 
 import org.lwjgl.opengl.GL11;
 
+import javax.annotation.Nullable;
+
 import io.github.elytra.movingworld.common.chunk.mobilechunk.MobileChunkClient;
 import io.github.elytra.movingworld.common.entity.EntityMovingWorld;
 
-public class RenderMovingWorld extends Render {
+public class RenderMovingWorld extends Render<EntityMovingWorld> {
     public RenderMovingWorld(RenderManager renderManager) {
         super(renderManager);
         shadowSize = 1F;
+    }
+
+    @Nullable
+    @Override
+    protected ResourceLocation getEntityTexture(EntityMovingWorld entity) {
+        return TextureMap.LOCATION_BLOCKS_TEXTURE;
     }
 
     public void renderVehicle(EntityMovingWorld entity, double x, double y, double z, float yaw, float partialTicks) {
@@ -41,18 +48,14 @@ public class RenderMovingWorld extends Render {
     }
 
     @Override
-    public void doRender(Entity entity, double x, double y, double z, float yaw, float partialTicks) {
-        renderVehicle((EntityMovingWorld) entity, x, y, z, yaw, partialTicks);
+    public void doRender(EntityMovingWorld entity, double x, double y, double z, float yaw, float partialTicks) {
+        renderVehicle(entity, x, y, z, yaw, partialTicks);
         super.doRender(entity, x, y, z, yaw, partialTicks);
     }
 
     @Override
-    public boolean shouldRender(Entity entity, ICamera camera, double camX, double camY, double camZ) {
-        return entity.isInRangeToRender3d(camX, camY, camZ);
+    public boolean shouldRender(EntityMovingWorld entity, ICamera camera, double camX, double camY, double camZ) {
+        return true;
     }
 
-    @Override
-    protected ResourceLocation getEntityTexture(Entity entity) {
-        return TextureMap.LOCATION_BLOCKS_TEXTURE;
-    }
 }
