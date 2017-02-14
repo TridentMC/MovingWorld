30ee168e8cab00461e92909b669f98c53ca9bb6b
darkevilmac <bk1325@gmail.com>
2017-02-13 21:15:39 -0800

    Small additions to EntityMobileRegion

--- src/test/java/com/elytradev/movingworld/common/experiments/entity/EntityMobileRegion.java
+++ src/test/java/com/elytradev/movingworld/common/experiments/entity/EntityMobileRegion.java
@@ -1,9 +1,15 @@
 package com.elytradev.movingworld.common.experiments.entity;
 
+import com.elytradev.movingworld.client.experiments.MobileRegionWorldClient;
 import com.elytradev.movingworld.common.experiments.MobileRegion;
+import com.elytradev.movingworld.common.experiments.MobileRegionWorldServer;
 import net.minecraft.entity.Entity;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldSettings;
+import net.minecraftforge.common.DimensionManager;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 /**
  * Experimental MobileRegion entity.
@@ -11,14 +17,50 @@ import net.minecraft.world.World;
 public class EntityMobileRegion extends Entity {
 
     public MobileRegion region;
+    public World mobileRegionWorld;
 
-    public EntityMobileRegion(World worldIn) {
+    public EntityMobileRegion(World worldIn, MobileRegion region) {
         super(worldIn);
+        this.region = region;
     }
 
     @Override
     protected void entityInit() {
+        if (world.isRemote) {
+            entityInitClient();
+        } else {
+            entityInitCommon();
+        }
+    }
 
+    protected void entityInitCommon() {
+        this.mobileRegionWorld = new MobileRegionWorldServer(getParentWorld().getMinecraftServer(),
+                getParentWorld().getSaveHandler(),
+                getParentWorld().getWorldInfo(),
+                region.dimension, getParentWorld().profiler);
+    }
+
+    @SideOnly(Side.CLIENT)
+    protected void entityInitClient() {
+        this.mobileRegionWorld = new MobileRegionWorldClient(null, genWorldSettings(),
+                region.dimension, getParentWorld().getDifficulty(), getParentWorld().profiler);
+
+        // TODO: Request data from server or make the server figure it out itself. Not sure yet.
+    }
+
+
+    private WorldSettings genWorldSettings() {
+        WorldSettings settings = new WorldSettings(0L,
+                getParentWorld().getWorldInfo().getGameType(),
+                false,
+                getParentWorld().getWorldInfo().isHardcoreModeEnabled(),
+                getParentWorld().getWorldType());
+
+        return settings;
+    }
+
+    public World getParentWorld() {
+        return DimensionManager.getWorld(region.dimension);
     }
 
     @Override
