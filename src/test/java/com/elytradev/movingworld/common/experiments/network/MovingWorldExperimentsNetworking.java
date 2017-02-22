1bf25f681d40ca094c22f3b71e387d92ea70322d
darkevilmac <bk1325@gmail.com>
2017-02-21 20:06:37 -0800

    Register packets that I forgot about.

--- src/test/java/com/elytradev/movingworld/common/experiments/network/MovingWorldExperimentsNetworking.java
+++ src/test/java/com/elytradev/movingworld/common/experiments/network/MovingWorldExperimentsNetworking.java
@@ -1,6 +1,9 @@
 package com.elytradev.movingworld.common.experiments.network;
 
 import com.elytradev.movingworld.common.experiments.MovingWorldExperimentsMod;
+import com.elytradev.movingworld.common.experiments.network.messages.client.MessageRequestData;
+import com.elytradev.movingworld.common.experiments.network.messages.server.MessageBlockData;
+import com.elytradev.movingworld.common.experiments.network.messages.server.MessageDimensionPoolData;
 import com.elytradev.movingworld.common.experiments.network.messages.server.MessageFullPoolData;
 import com.elytradev.movingworld.common.experiments.network.messages.server.MessageRegionData;
 import io.github.elytra.concrete.NetworkContext;
@@ -16,11 +19,13 @@ public class MovingWorldExperimentsNetworking {
         networkContext = NetworkContext.forChannel(MovingWorldExperimentsMod.MOD_ID);
 
         // Register packets bound to server.
-        networkContext.register(MessageRequestRegionData.class);
+        networkContext.register(MessageRequestData.class);
 
         // Register packets bound to client.
         networkContext.register(MessageRegionData.class);
         networkContext.register(MessageFullPoolData.class);
+        networkContext.register(MessageDimensionPoolData.class);
+        networkContext.register(MessageBlockData.class);
     }
 
 }
