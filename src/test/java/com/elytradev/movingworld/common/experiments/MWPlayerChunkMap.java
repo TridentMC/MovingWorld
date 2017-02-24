928c70ca4d35a6fec1089e7ac1c868729aeb00a6
darkevilmac <bk1325@gmail.com>
2017-02-23 23:57:36 -0800

    somethingsomething implement methods in stub classes somethingsomething

--- src/test/java/com/elytradev/movingworld/common/experiments/MWPlayerChunkMap.java
+++ src/test/java/com/elytradev/movingworld/common/experiments/MWPlayerChunkMap.java
@@ -12,8 +12,30 @@ public class MWPlayerChunkMap extends PlayerChunkMap {
         super(serverWorld);
     }
 
+    // just a copy of the same method found in the vanilla PlayerChunkMap.
+    private static long getIndex(int p_187307_0_, int p_187307_1_) {
+        return (long) p_187307_0_ + 2147483647L | (long) p_187307_1_ + 2147483647L << 32;
+    }
+
     @Override
     public PlayerChunkMapEntry getOrCreateEntry(int chunkX, int chunkZ) {
-        return super.getOrCreateEntry(chunkX, chunkZ);
+        long i = getIndex(chunkX, chunkZ);
+        PlayerChunkMapEntry playerchunkmapentry = this.entryMap.get(i);
+
+        if (playerchunkmapentry == null) {
+            playerchunkmapentry = new MWPlayerChunkMapEntry(this, chunkX, chunkZ);
+            this.entryMap.put(i, playerchunkmapentry);
+            this.entries.add(playerchunkmapentry);
+
+            if (playerchunkmapentry.getChunk() == null) {
+                this.entriesWithoutChunks.add(playerchunkmapentry);
+            }
+
+            if (!playerchunkmapentry.sendToPlayers()) {
+                this.pendingSendToPlayers.add(playerchunkmapentry);
+            }
+        }
+
+        return playerchunkmapentry;
     }
 }
