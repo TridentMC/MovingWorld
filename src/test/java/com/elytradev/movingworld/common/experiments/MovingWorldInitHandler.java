33382936840302e48df05b11eda0c98a77ad9d1d
darkevilmac <bk1325@gmail.com>
2017-03-15 23:12:32 -0700

    Use accessor in MovingWorldInitHandler, some code commenting, set WorldEventHandler.

--- src/test/java/com/elytradev/movingworld/common/experiments/MovingWorldInitHandler.java
+++ src/test/java/com/elytradev/movingworld/common/experiments/MovingWorldInitHandler.java
@@ -1,16 +1,21 @@
 package com.elytradev.movingworld.common.experiments;
 
+import com.elytradev.concrete.reflect.accessor.Accessor;
+import com.elytradev.concrete.reflect.accessor.Accessors;
 import com.elytradev.movingworld.client.experiments.InputReader;
 import com.elytradev.movingworld.client.experiments.MovingWorldClientDatabase;
 import com.elytradev.movingworld.common.experiments.network.messages.server.MessageDimensionPoolData;
 import com.elytradev.movingworld.common.experiments.network.messages.server.MessageFullPoolData;
 import com.elytradev.movingworld.common.experiments.region.RegionPool;
 import com.elytradev.movingworld.common.experiments.world.MWPlayerChunkMap;
+import com.elytradev.movingworld.common.experiments.world.MWServerWorldEventHandler;
 import com.elytradev.movingworld.common.experiments.world.MovingWorldProvider;
 import com.google.common.collect.HashBiMap;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.WorldClient;
+import net.minecraft.server.management.PlayerChunkMap;
 import net.minecraft.world.DimensionType;
+import net.minecraft.world.ServerWorldEventHandler;
 import net.minecraft.world.WorldServer;
 import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.common.MinecraftForge;
@@ -18,9 +23,7 @@ import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
-import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
-import java.lang.reflect.Field;
 import java.util.HashMap;
 
 /**
@@ -32,6 +35,13 @@ public class MovingWorldInitHandler {
     public static int startingDimID = 50;
     public static int activeDimID = startingDimID;
 
+    private Accessor<PlayerChunkMap> playerChunkMap;
+
+
+    public MovingWorldInitHandler() {
+        this.playerChunkMap = Accessors.findField(WorldServer.class, "playerChunkMap", "field_73063_M");
+    }
+
     @SubscribeEvent
     public void onWorldLoad(WorldEvent.Load e) {
         Integer loadedDimensionID = e.getWorld().provider.getDimension();
@@ -42,15 +52,22 @@ public class MovingWorldInitHandler {
         }
 
         try {
+            // Register new dimension
             registeredDimensions.put(loadedDimensionID, activeDimID);
             DimensionManager.registerDimension(activeDimID,
                     DimensionType.register("MovingWorld|P" + loadedDimensionID + "|C" + activeDimID,
                             "movingworld", activeDimID, MovingWorldProvider.class, true));
             DimensionManager.initDimension(activeDimID);
             WorldServer worldServer = DimensionManager.getWorld(activeDimID);
-            Field playerChunkMap = ReflectionHelper.findField(WorldServer.class, "playerChunkMap", "field_73063_M");
-            playerChunkMap.setAccessible(true);
+
+            // Inject set MW component alternatives.
             playerChunkMap.set(worldServer, new MWPlayerChunkMap(worldServer));
+            ServerWorldEventHandler currentEventListener = (ServerWorldEventHandler) worldServer.eventListeners.stream()
+                    .filter((ev) -> ev instanceof ServerWorldEventHandler).findFirst().get();
+            worldServer.eventListeners.remove(currentEventListener);
+            worldServer.addEventListener(new MWServerWorldEventHandler(worldServer.mcServer, worldServer));
+
+            // Init pool, increment dimension number.
             RegionPool.getPool(activeDimID, true);
             worldServer.addEventListener(new BoundingBoxWorldListener());
             MovingWorldExperimentsMod.logger.info("DB check: " + MovingWorldExperimentsMod.modProxy.getCommonDB().getWorldFromDim(activeDimID));
