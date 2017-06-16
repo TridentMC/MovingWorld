14979e15eab7fdebb0387205d70e5448249d215f
darkevilmac <bk1325@gmail.com>
2017-06-16 01:31:30 -0700

    Changes to MobileChunkRenderer, will most likely convert to VBOs before a release.
    Update Forge, as well as Mappings.

--- src/main/java/com/elytradev/movingworld/client/render/MobileChunkRenderer.java
+++ src/main/java/com/elytradev/movingworld/client/render/MobileChunkRenderer.java
@@ -2,6 +2,7 @@ package com.elytradev.movingworld.client.render;
 
 import com.elytradev.movingworld.MovingWorldMod;
 import com.elytradev.movingworld.common.chunk.mobilechunk.MobileChunk;
+import com.google.common.collect.Maps;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
@@ -12,6 +13,7 @@ import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.util.Tuple;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.client.MinecraftForgeClient;
@@ -19,107 +21,133 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.lwjgl.opengl.GL11;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @SideOnly(Side.CLIENT)
 public class MobileChunkRenderer {
-	/**
-	 * Boolean for whether this renderer needs to be updated or not
-	 */
-	public boolean needsUpdate;
-	public boolean isRemoved;
+    /**
+     * Boolean for whether this renderer needs to be updated or not
+     */
+    public boolean needsUpdate;
+    public boolean isRemoved;
 
-	private MobileChunk chunk;
+    private int displayList;
 
-	public MobileChunkRenderer(MobileChunk mobilechunk) {
-		chunk = mobilechunk;
-		needsUpdate = true;
-	}
+    private MobileChunk chunk;
 
-	public void render(float partialTicks) {
-		if (needsUpdate) {
-			try {
-				updateSimpleRender(partialTicks);
-			} catch (Exception e) {
-				MovingWorldMod.LOG.error("Exception when rendering a MobileChunk! ", e);
-			}
-		}
-	}
+    public MobileChunkRenderer(MobileChunk mobilechunk) {
+        chunk = mobilechunk;
+        needsUpdate = true;
+    }
 
-	private void updateSimpleRender(float partialTicks) {
-		Tessellator tessellator = Tessellator.getInstance();
-		BufferBuilder buffer = tessellator.getBuffer();
+    public void render(float partialTicks) {
+        if (isRemoved) {
+            GLAllocation.deleteDisplayLists(displayList);
+            return;
+        }
 
-		RenderHelper.disableStandardItemLighting();
-		GlStateManager.blendFunc(770, 771);
-		GlStateManager.enableBlend();
-		GlStateManager.enableCull();
+        try {
+            if (needsUpdate)
+                compileSimpleRender();
 
-		if (Minecraft.isAmbientOcclusionEnabled()) {
-			GlStateManager.shadeModel(7425);
-		} else {
-			GlStateManager.shadeModel(7424);
-		}
+            GlStateManager.callList(this.displayList);
 
-		buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.BLOCK);
-		for (int y = chunk.minY(); y < chunk.maxY(); ++y) {
-			for (int z = chunk.minZ(); z < chunk.maxZ(); ++z) {
-				for (int x = chunk.minX(); x < chunk.maxX(); ++x) {
-					BlockPos pos = new BlockPos(x, y, z);
-					IBlockState blockState = chunk.getBlockState(pos);
-					Block block = blockState.getBlock();
+            renderTiles(partialTicks);
+        } catch (Exception e) {
+            MovingWorldMod.LOG.error("Exception when rendering a MobileChunk! ", e);
+        }
 
-					for (BlockRenderLayer blockRenderLayer : BlockRenderLayer.values()) {
-						if (!block.canRenderInLayer(blockState, blockRenderLayer)) continue;
-						net.minecraftforge.client.ForgeHooksClient.setRenderLayer(blockRenderLayer);
+    }
 
-						if (!block.getRenderType(blockState).equals(EnumBlockRenderType.INVISIBLE)) {
-							dispatchBlockRender(blockState, pos, buffer);
-						}
-					}
-					net.minecraftforge.client.ForgeHooksClient.setRenderLayer(null);
-				}
-			}
-		}
-		buffer.setTranslation(0.0D, 0.0D, 0.0D);
-		tessellator.draw();
-		GlStateManager.disableBlend();
-		GlStateManager.disableCull();
-		RenderHelper.enableStandardItemLighting();
+    private void compileSimpleRender() {
+        this.displayList = GLAllocation.generateDisplayLists(1);
+        GlStateManager.glNewList(this.displayList, 4864);
 
-		GlStateManager.pushMatrix();
-		World tesrDispatchWorld = TileEntityRendererDispatcher.instance.world;
-		TileEntityRendererDispatcher.instance.setWorld(chunk.getFakeWorld());
-		for (int y = chunk.minY(); y < chunk.maxY(); ++y) {
-			for (int z = chunk.minZ(); z < chunk.maxZ(); ++z) {
-				for (int x = chunk.minX(); x < chunk.maxX(); ++x) {
-					BlockPos pos = new BlockPos(x, y, z);
-					TileEntity tile = chunk.getTileEntity(pos);
-					if (tile != null) {
-						tile.setWorld(chunk.getFakeWorld());
-						TileEntitySpecialRenderer renderer = TileEntityRendererDispatcher.instance.getSpecialRenderer(tile);
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder buffer = tessellator.getBuffer();
 
-						if (renderer != null && tile.shouldRenderInPass(MinecraftForgeClient.getRenderPass())) {
-							TileEntityRendererDispatcher.instance.renderTileEntityAt(tile, tile.getPos().getX(), tile.getPos().getY(), tile.getPos().getZ(), partialTicks);
-						}
-						tile.setWorld(chunk.world);
-					}
-				}
-			}
-		}
-		TileEntityRendererDispatcher.instance.setWorld(tesrDispatchWorld);
-		GlStateManager.popMatrix();
-	}
+        RenderHelper.disableStandardItemLighting();
+        GlStateManager.blendFunc(770, 771);
+        GlStateManager.enableBlend();
+        GlStateManager.enableCull();
 
-	public void dispatchBlockRender(IBlockState blockState, BlockPos blockPos, BufferBuilder buffer) {
-		buffer.color(1.0F, 1.0F, 1.0F, 1.0F);
-		BlockRendererDispatcher blockRendererDispatcher = Minecraft.getMinecraft().getBlockRendererDispatcher();
-		blockRendererDispatcher.renderBlock(blockState, blockPos, chunk.getFakeWorld(), buffer);
-	}
+        if (Minecraft.isAmbientOcclusionEnabled()) {
+            GlStateManager.shadeModel(7425);
+        } else {
+            GlStateManager.shadeModel(7424);
+        }
 
-	public void markDirty() {
-		needsUpdate = true;
-	}
+        HashMap<BlockRenderLayer, List<Tuple<BlockPos, IBlockState>>> blockRenderMap = Maps.newHashMap();
+        for (BlockRenderLayer blockRenderLayer : BlockRenderLayer.values()) {
+            blockRenderMap.put(blockRenderLayer, new ArrayList());
+        }
+        // Collect block states.
+        for (int y = chunk.minY(); y < chunk.maxY(); ++y) {
+            for (int z = chunk.minZ(); z < chunk.maxZ(); ++z) {
+                for (int x = chunk.minX(); x < chunk.maxX(); ++x) {
+                    BlockPos pos = new BlockPos(x, y, z);
+                    IBlockState blockState = chunk.getBlockState(pos);
+                    Block block = blockState.getBlock();
 
-	public void markRemoved() {
-		isRemoved = true;
-	}
+                    for (BlockRenderLayer blockRenderLayer : BlockRenderLayer.values()) {
+                        if (!block.canRenderInLayer(blockState, blockRenderLayer)
+                                || blockState.getRenderType().equals(EnumBlockRenderType.INVISIBLE)) continue;
+
+                        blockRenderMap.get(blockRenderLayer).add(new Tuple<>(pos, blockState));
+                    }
+                }
+            }
+        }
+
+        buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.BLOCK);
+        // Actually render.
+        for (BlockRenderLayer blockRenderLayer : BlockRenderLayer.values()) {
+            for (Tuple<BlockPos, IBlockState> blockRenderData : blockRenderMap.get(blockRenderLayer)) {
+                dispatchBlockRender(blockRenderData.getFirst(), blockRenderData.getSecond(), buffer);
+            }
+        }
+
+        buffer.setTranslation(0.0D, 0.0D, 0.0D);
+        tessellator.draw();
+        GlStateManager.disableBlend();
+        GlStateManager.disableCull();
+        RenderHelper.enableStandardItemLighting();
+
+        GlStateManager.glEndList();
+        this.needsUpdate = false;
+    }
+
+    private void renderTiles(float partialTicks) {
+        GlStateManager.pushMatrix();
+        World tesrDispatchWorld = TileEntityRendererDispatcher.instance.world;
+        TileEntityRendererDispatcher.instance.setWorld(chunk.getFakeWorld());
+        for (Map.Entry<BlockPos, TileEntity> blockPosTileEntityEntry : chunk.chunkTileEntityMap.entrySet()) {
+            TileEntity tile = blockPosTileEntityEntry.getValue();
+            tile.setWorld(chunk.getFakeWorld());
+            TileEntitySpecialRenderer renderer = TileEntityRendererDispatcher.instance.getSpecialRenderer(tile);
+            if (renderer != null && tile.shouldRenderInPass(MinecraftForgeClient.getRenderPass())) {
+                TileEntityRendererDispatcher.instance.renderTileEntityAt(tile, tile.getPos().getX(), tile.getPos().getY(), tile.getPos().getZ(), partialTicks);
+            }
+            tile.setWorld(chunk.world);
+        }
+        TileEntityRendererDispatcher.instance.setWorld(tesrDispatchWorld);
+        GlStateManager.popMatrix();
+    }
+
+    public void dispatchBlockRender(BlockPos blockPos, IBlockState blockState, BufferBuilder buffer) {
+        buffer.color(1.0F, 1.0F, 1.0F, 1.0F);
+        BlockRendererDispatcher blockRendererDispatcher = Minecraft.getMinecraft().getBlockRendererDispatcher();
+        blockRendererDispatcher.renderBlock(blockState, blockPos, chunk.getFakeWorld(), buffer);
+    }
+
+    public void markDirty() {
+        needsUpdate = true;
+    }
+
+    public void markRemoved() {
+        isRemoved = true;
+    }
 }
