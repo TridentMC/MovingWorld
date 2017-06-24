5ba7d0385d4e9c5d104363dcc2c882f16731f721
darkevilmac <bk1325@gmail.com>
2017-06-24 02:37:45 -0700

    Fix bad gradle stuff, jars are made properly now. Moved experiments into zip file for now, need to put it in a branch.

--- src/test/java/com/elytradev/movingworld/common/experiments/newassembly/WorldReadResult.java
+++
@@ -1,7 +0,0 @@
-package com.elytradev.movingworld.common.experiments.newassembly;
-
-/**
- * Created by darkevilmac on 11/15/2016.
- */
-public class WorldReadResult {
-}
--- src/test/java/com/elytradev/movingworld/common/experiments/newassembly/WorldWriter.java
+++
@@ -1,7 +0,0 @@
-package com.elytradev.movingworld.common.experiments.newassembly;
-
-/**
- * Used to write a block collection to a world.
- */
-public class WorldWriter {
-}
